aa316c8c0dd47f27cb76cc7dc49ba0567a4a261a
IbraDirir <khaliildirir@gmail.com>
2015-09-15 11:27:58 +0300

    Loops in java

---
+++ src/loops/Loops.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0;
+		while (x<20){
+			x++;
+			System.out.println(x);
+		}
+		
+		int counter;
+		for (counter = 1;counter<=20;counter++){
+			System.out.println(counter);
+		}
+	}
+	
+}
